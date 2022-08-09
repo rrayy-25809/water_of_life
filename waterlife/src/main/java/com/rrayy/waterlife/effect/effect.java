12d495a4c4829de44488f0175574dfeb81b405db
rrayy <72653431+rrayy-25809@users.noreply.github.com>
2022-08-09 22:58:56 +0900

    Update effect.java

--- waterlife/src/main/java/com/rrayy/waterlife/effect/effect.java
+++ waterlife/src/main/java/com/rrayy/waterlife/effect/effect.java
@@ -1,5 +1,11 @@
 package com.rrayy.waterlife.effect;
 
+import com.rrayy.waterlife.waterlife;
+
 public class effect {
-    
+    private waterlife plugin;
+    public text_manager(waterlife plugin) {
+        this.plugin = plugin;
+    }
+    public give_effect()
 }
